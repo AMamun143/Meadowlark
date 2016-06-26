18b9f628c5de4c3968539bf0b833c7d49ebb41e4
Mamunnyc <Mamun0320@gmail.com>
2016-06-26 11:31:25 -0400

    Initial commit.

---
+++ site/app.js
@@ -0,0 +1,52 @@
+var express = require ('express');
+var app = express();
+
+//set up handlebars view engine
+var handlebars = require('express-handlebars')
+		.create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+app.set('port', process.env.PORT || 3000);
+
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+// routes
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	var randomFortune = 
+			fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', { fortune: randomFortune});
+});
+
+
+// 404 catch-all handler (middlewares)
+app.use(function (req, res, next) {
+ 	res.status(404);
+	res.render('404');
+});
+
+// 500 error handler (middlewares)
+app.use(function (err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+var fortunes = [
+		"Conquer your fears or they will conquer you.",
+		"Rivers need springs",
+		"Do not fear what you don't know",
+		"you will have a pleasant serprise",
+		"Whenever possible, keep it simple",
+];
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' +
+		app.get('port') + '; press Ctrl-c to terminate');
+});
