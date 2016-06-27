8245df6f1eb3a69db82eb30d736023aecde67bd6
Mamunnyc <Mamun0320@gmail.com>
2016-06-27 19:22:38 -0400

    installed zoomie, mocha, chain and grunt

--- site/app.js
+++ site/app.js
@@ -1,53 +1,64 @@
-var fortune = require('./lib/fortune.js')
-var express = require ('express');
+var express = require('express');
+var fortune = require('./lib/fortune.js');
+
 var app = express();
 
-//set up handlebars view engine
-var handlebars = require('express-handlebars')
-		.create({defaultLayout:'main'});
+// set up handlebars view engine
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-
 app.set('port', process.env.PORT || 3000);
 
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
+// set 'showTests' context property if the querystring contains test=1
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && 
+		req.query.test === '1';
+	next();
+});
 
-// var fortunes = [
-// 		"Conquer your fears or they will conquer you.",
-// 		"Rivers need springs",
-// 		"Do not fear what you don't know",
-// 		"you will have a pleasant serprise",
-// 		"Whenever possible, keep it simple",
-// ];
-
-// routes
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	res.render('home');
 });
-
-app.get('/about', function(req, res){
-	// var randomFortune = 
-	// 		fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about', { fortune: fortune.getFortune()});
+app.get('/about', function(req,res){
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js' 
+	} );
+});
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
 });
 
-
-// 404 catch-all handler (middlewares)
-app.use(function (req, res, next) {
- 	res.status(404);
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
 	res.render('404');
 });
 
-// 500 error handler (middlewares)
-app.use(function (err, req, res, next){
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
 	console.error(err.stack);
 	res.status(500);
 	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
-	console.log('Express started on http://localhost:' +
-		app.get('port') + '; press Ctrl-c to terminate');
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
 });
